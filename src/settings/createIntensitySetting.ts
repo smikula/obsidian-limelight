a16b7bd3c8d523dc25bf1573e564dabaa5bc4e2c
Scott Mikula <mikula@gmail.com>
2021-10-23 08:07:40 -0700

    Apply intensity via a CSS variable

--- src/settings/createIntensitySetting.ts
+++ src/settings/createIntensitySetting.ts
@@ -11,6 +11,17 @@ export function createIntensitySetting(container: HTMLElement, data: LimelightDa
                 .onChange((value) => {
                     // Write new value to settings
                     data.setField('intensity', value);
+
+                    // Apply to UI
+                    document.body.style.setProperty('--limelight-opacity', toOpacity(value));
                 })
         );
+}
+
+function toOpacity(intensity: number) {
+    // Opacity ranges from 0.1 - 0.9, with MORE intense having a LOWER opacity
+    // value (i.e. the other panes appear dimmer because the spotlight is so
+    // bright).
+    const opacity = 0.9 - 0.8 * (intensity / 100);
+    return opacity.toString();
 }
