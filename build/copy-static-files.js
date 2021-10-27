204542f81041994815878cb497e052d2c2e8dd1b
Scott Mikula <mikula@gmail.com>
2021-10-27 10:53:53 -0700

    Move manifest.json back to the root of the repo

--- build/copy-static-files.js
+++ build/copy-static-files.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
-fs.copyFileSync('./src/manifest.json', './dist/manifest.json');
+fs.copyFileSync('./manifest.json', './dist/manifest.json');
 fs.copyFileSync('./src/styles.css', './dist/styles.css');
